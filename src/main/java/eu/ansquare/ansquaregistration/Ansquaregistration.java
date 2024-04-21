5bb2e638c735f306a5272287aab3465fad5274d0
farpo <77583105+farpo@users.noreply.github.com>
2024-04-21 18:50:01 +0200

    Initial commit

---
+++ src/main/java/eu/ansquare/ansquaregistration/Ansquaregistration.java
@@ -0,0 +1,15 @@
+package eu.ansquare.ansquaregistration;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class Ansquaregistration implements ModInitializer {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    @Override
+    public void onInitialize(ModContainer mod) {
+
+    }
+}
